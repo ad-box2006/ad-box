c3dee56208ba82732bb5d9cfacef21fcee391a5e
katlegokirra <katlegokirra@gmail.com>
2026-08-07 16:18:13 +0200

    yes

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ try:
 except:
     font = ImageFont.load_default()
 text = "Ad"
-text_w, text_h = draw.textlength(text, font=font)
+bbox = draw.textbbox((0, 0), text, font=font)
+text_w = bbox[2] - bbox[0]
+text_h = bbox[3] - bbox[1]
 x = (img.width - text_w) // 2
 y = (img.height - text_h) // 2
 draw.text((x, y), text, font=font, fill=(255, 255, 255, 255))
